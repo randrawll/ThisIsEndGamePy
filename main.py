e4fe53cc06726c36014a021bd6d4e4ab0e029f4b
Randall <randall@Randalls-MacBook-Pro.local>
2022-10-31 11:43:11 -0700

    refactored code quite a bit

--- main.py
+++ main.py
@@ -4,74 +4,52 @@ from level import Level
 from settings import *
 from pygame.locals import (K_UP,K_DOWN,K_LEFT,K_RIGHT,K_SPACE,K_ESCAPE,KEYDOWN,KEYUP,QUIT)
 
-moveSpeed = 10
+moveSpeed = 1
 
-class Camera:
-    def __init__(self, w, h):
-        self.camera = pygame.Rect(0, 0, w, h)
-        self.width = w
-        self.height = h
+class Game():
+    def __init__(self):  
+        pygame.init()
+        self.screen = pygame.display.set_mode([SCREEN_WIDTH , SCREEN_HEIGHT])
+        self.clock = pygame.time.Clock()
 
-    def apply(self, e):
-        return e.rect.move(self.camera.topleft)
+    def initialize(self):
+        self.gameSprites = pygame.sprite.Group()
+        self.player = Player(self, 10, 10)
+        self.level = Level(SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+    def run(self):
+        self.running = True
+        while self.running:
+            self.dt = self.clock.tick(60) / 1000
+            self.events()
+            self.update(self.dt)
+            self.draw()
 
-    def update(self, target, width, height):
-        x = -target.rect.x + int(width / 2)
-        y = -target.rect.y + int(height / 2)
-        self.camera = pygame.Rect(x, y, self.width, self.height)
+    def events(self):
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT: pygame.quit() 
+    
+    def update(self, dt):
+        self.player.move(dt)
+        self.gameSprites.update()
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode([SCREEN_WIDTH , SCREEN_HEIGHT])
-    player = Player(10, 10)
-    level = Level(SCREEN_WIDTH, SCREEN_HEIGHT)
-    running = True
-    x_velo, y_velo = 0, 0
-    clock = pygame.time.Clock()
-    #cam = Camera()
-
-    while running:
-        screen.fill((0, 0, 0))
-
-        checkKeys(player) 
-        #checkBounds(player)
-
-        #level.draw(screen)
-        level.drawgrid(screen)
-
-
-        player.draw(screen)
-
-        #cam.update(player)
-
-        pygame.display.update()
+    def draw(self):
+        self.screen.fill((0,0,0))
+        self.level.drawgrid(self.screen)
+        self.gameSprites.draw(self.screen)
         pygame.display.flip()
-        clock.tick(30)
 
-def checkKeys(player):
-    klist = pygame.key.get_pressed()
-    if pygame.key.get_pressed()[K_UP]:
-        player.y += moveSpeed * -1
-    if pygame.key.get_pressed()[K_DOWN]:
-        player.y += moveSpeed 
-    if pygame.key.get_pressed()[K_LEFT]:
-        player.x += moveSpeed * -1
-    if pygame.key.get_pressed()[K_RIGHT]:
-        player.x += moveSpeed
-    if pygame.key.get_pressed()[K_SPACE]:
-        player.attack()
-    for e in pygame.event.get():
-        #if e.type == pygame.KEYDOWN: 
-        #if e.type == pygame.KEYUP:     return 0
-        if e.type == pygame.QUIT: pygame.quit()   
 
-def checkBounds(thing):
-    if thing.x > SCREEN_WIDTH - TILESIZE:   thing.x = SCREEN_WIDTH - TILESIZE - 1
-    if thing.x < 0:                         thing.x = 1
-    if thing.y > SCREEN_HEIGHT - TILESIZE:  thing.y = SCREEN_HEIGHT - TILESIZE - 1
-    if thing.y < 0:                         thing.y = 1
+    
+#player.draw(screen)
+#pygame.display.update()
+    
+g = Game()
+g.initialize()
+while True:
+    g.run()
+  
 
-main()
 
 
 
--- player.py
+++ player.py
@@ -1,26 +1,65 @@
 import pygame
 from settings import *
+from pygame.locals import (K_UP,K_DOWN,K_LEFT,K_RIGHT,K_SPACE,K_ESCAPE,KEYDOWN,KEYUP,QUIT)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super(Player, self)
-        self.x = x 
-        self.y = y
-        self.hp = 100
+    def __init__(self, game, x, y):
+        self.groups = game.gameSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
         self.image = pygame.Surface((TILESIZE, TILESIZE))
         self.image.fill(BLUE)
         self.rect = self.image.get_rect()
+        self.x = x 
+        self.y = y
 
         #self.playerImage = pygame.image.load("duck.png").convert()
         #self.playerImage.set_colorkey((0, 0, 0))
         #self.rect = self.playerImage.get_rect()
-
-    def draw(self, screen): 
+    
+    def update(self):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
+
+
+    def draw(self, screen): 
         screen.blit(self.image, self.rect)
         #self.rect = self.playerImage.get_rect()
         #self.rect.move_ip(self.x, self.y)
         #screen.blit(self.playerImage, self.rect)
 
+    def move(self, dt):
+        self.klist = pygame.key.get_pressed()
+        if pygame.key.get_pressed()[K_UP]:
+            self.y += -1 * dt * TILESIZE
+        if pygame.key.get_pressed()[K_DOWN]:
+            self.y += dt * TILESIZE
+        if pygame.key.get_pressed()[K_LEFT]:
+            self.x += -1 * dt * TILESIZE
+        if pygame.key.get_pressed()[K_RIGHT]:
+            self.x += dt * TILESIZE
+        #if pygame.key.get_pressed()[K_SPACE]:
+        #    self.attack()
 
+    def checkKeys(player):
+        klist = pygame.key.get_pressed()
+        if pygame.key.get_pressed()[K_UP]:
+            player.y += MOVESPEED * -1
+        if pygame.key.get_pressed()[K_DOWN]:
+            player.y += MOVESPEED 
+        if pygame.key.get_pressed()[K_LEFT]:
+            player.x += MOVESPEED * -1
+        if pygame.key.get_pressed()[K_RIGHT]:
+            player.x += MOVESPEED
+        if pygame.key.get_pressed()[K_SPACE]:
+            player.attack()
+        for e in pygame.event.get():
+            #if e.type == pygame.KEYDOWN: 
+            #if e.type == pygame.KEYUP:     return 0
+            if e.type == pygame.QUIT: pygame.quit() 
+
+    def checkBounds(thing):
+        if thing.x > SCREEN_WIDTH - TILESIZE:   thing.x = SCREEN_WIDTH - TILESIZE - 1
+        if thing.x < 0:                         thing.x = 1
+        if thing.y > SCREEN_HEIGHT - TILESIZE:  thing.y = SCREEN_HEIGHT - TILESIZE - 1
+        if thing.y < 0:                         thing.y = 1
